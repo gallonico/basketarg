ce518fb97b0aa93e5286456735f1bd5675ded7b3
gallonico-pc <nicolasgallo98@hotmail.com>
2026-04-26 10:36:51 -0300

    Mejora código

--- data-raw/carga-datos.R
+++ data-raw/carga-datos.R
@@ -1,12 +1,32 @@
-# Cargar los archivos de excel
+# ============================================================
+# LIBRERÍAS
+# ============================================================
+
 library(readxl)
+library(usethis)
+
+
+# ============================================================
+# CARGA Y PREPARACIÓN DE DATOS PARA EL PAQUETE
+# ============================================================
+
+# ------------------------------
+# Carga de archivos Excel
+# ------------------------------
+
 equipos <- read_excel("data-raw/equipos.xlsx")
 jugadores <- read_excel("data-raw/jugadores.xlsx")
 
-# Eliminar variables innecesarias
+# ------------------------------
+# Eliminación de variables auxiliares innecesarias
+# ------------------------------
+
 equipos <- equipos[, !names(equipos) %in% c("source", "df")]
+
 jugadores <- jugadores[, !names(jugadores) %in% c("source", "df")]
 
-# Guardar los datasets en formato .rda dentro de la carpeta data/
-library(usethis)
+# ------------------------------
+# Guardado de datasets en formato .rda
+# ------------------------------
+
 use_data(equipos, jugadores, overwrite = TRUE)
